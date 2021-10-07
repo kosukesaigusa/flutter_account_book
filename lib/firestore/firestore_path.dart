8d132f861c8ec5ed15c1bd3353f9162b7bbe3a26
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-10-08 01:11:59 +0900

    fix: small fix

--- lib/firestore/firestore_path.dart
+++ lib/firestore/firestore_path.dart
@@ -4,7 +4,7 @@ import 'package:flutter_account_book/store/store.dart';
 class FirestorePath {
   final store = Store();
   static CollectionReference<Map<String, dynamic>> expenseCollectionRef() =>
-      Store().db.collection('expensesV1').doc(Store().currentUser.uid).collection('expenses');
+      Store().db.collection('expenseV1').doc(Store().currentUser.uid).collection('expenses');
   static CollectionReference<Map<String, dynamic>> cateogoryCollectionRef() =>
       Store().db.collection('categoryV1').doc(Store().currentUser.uid).collection('categories');
 }
