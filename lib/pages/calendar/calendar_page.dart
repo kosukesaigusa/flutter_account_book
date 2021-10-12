a5341686cae8c92f1b0ea0266c76c5f1dd33b6ba
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-10-12 19:07:50 +0900

    feat: implement pull to refresh

--- lib/pages/calendar/calendar_page.dart
+++ lib/pages/calendar/calendar_page.dart
@@ -3,25 +3,31 @@ import 'package:flutter_account_book/componentns/calendar/calendar_widget.dart';
 import 'package:flutter_account_book/componentns/calendar/expense_incom_total.dart';
 import 'package:flutter_account_book/componentns/calendar/expenses_of_day.dart';
 import 'package:flutter_account_book/componentns/calendar/header_month_handler.dart';
+import 'package:flutter_account_book/view_models/calendar/calendar_view_model.dart';
 
 class CalendarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          flexibleSpace: FlexibleSpaceBar(
-            title: CalendarMonthHandlingWidget(),
+    return RefreshIndicator(
+      onRefresh: () async {
+        await CalendarViewModel().fetchExpensesAndIncomes();
+      },
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            flexibleSpace: FlexibleSpaceBar(
+              title: CalendarMonthHandlingWidget(),
+            ),
           ),
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate([
-            ExpenseIncomeTotalWidget(),
-            CalendarWidget(),
-          ]),
-        ),
-        ExpensesOfDaySliverList(),
-      ],
+          SliverList(
+            delegate: SliverChildListDelegate([
+              ExpenseIncomeTotalWidget(),
+              CalendarWidget(),
+            ]),
+          ),
+          ExpensesOfDaySliverList(),
+        ],
+      ),
     );
   }
 }
--- lib/pages/category/category_page.dart
+++ lib/pages/category/category_page.dart
@@ -14,49 +14,54 @@ class CategoryPage extends StatelessWidget {
         appBar: AppBar(),
         body: Consumer<CategoryViewModel>(
           builder: (context, vm, child) {
-            return ReorderableListView.builder(
-              itemBuilder: (context, index) {
-                final expenseCategory = vm.expenseCategories[index];
-                return ListTile(
-                  key: Key(expenseCategory.reference!.id),
-                  title: Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          expenseCategory.name,
-                          overflow: TextOverflow.ellipsis,
+            return RefreshIndicator(
+              onRefresh: () async {
+                await CategoryViewModel().fetchCategories();
+              },
+              child: ReorderableListView.builder(
+                itemBuilder: (context, index) {
+                  final expenseCategory = vm.expenseCategories[index];
+                  return ListTile(
+                    key: Key(expenseCategory.reference!.id),
+                    title: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            expenseCategory.name,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
-                      ),
-                      const Gap(32),
-                      Text(
-                        '${priceFormatter.format(expenseCategory.budget)} 円',
-                        style: bold,
-                      ),
-                    ],
-                  ),
-                  trailing: const Icon(Icons.menu),
-                );
-              },
-              itemCount: vm.expenseCategories.length,
-              onReorder: (oldIndex, newIndex) async {
-                final batch = store.db.batch();
-                if (oldIndex < newIndex) {
-                  newIndex -= 1;
-                }
-                vm.expenseCategories.insert(newIndex, vm.expenseCategories.removeAt(oldIndex));
-                for (var i = 0; i < vm.expenseCategories.length; i++) {
-                  batch.update(
-                    vm.expenseCategories[i].reference!,
-                    <String, dynamic>{'order': i + 100},
+                        const Gap(32),
+                        Text(
+                          '${priceFormatter.format(expenseCategory.budget)} 円',
+                          style: bold,
+                        ),
+                      ],
+                    ),
+                    trailing: const Icon(Icons.menu),
                   );
-                }
-                try {
-                  await batch.commit();
-                  // ignore: avoid_catches_without_on_clauses
-                } catch (_) {
-                  showFloatingSnackBar(context, 'エラーが発生しました。');
-                }
-              },
+                },
+                itemCount: vm.expenseCategories.length,
+                onReorder: (oldIndex, newIndex) async {
+                  final batch = store.db.batch();
+                  if (oldIndex < newIndex) {
+                    newIndex -= 1;
+                  }
+                  vm.expenseCategories.insert(newIndex, vm.expenseCategories.removeAt(oldIndex));
+                  for (var i = 0; i < vm.expenseCategories.length; i++) {
+                    batch.update(
+                      vm.expenseCategories[i].reference!,
+                      <String, dynamic>{'order': i + 100},
+                    );
+                  }
+                  try {
+                    await batch.commit();
+                    // ignore: avoid_catches_without_on_clauses
+                  } catch (_) {
+                    showFloatingSnackBar(context, 'エラーが発生しました。');
+                  }
+                },
+              ),
             );
           },
         ),
