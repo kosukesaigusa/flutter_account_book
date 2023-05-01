13c234568263e1bcc717586ce771621a247312fd
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-05-02 02:57:40 +0900

    refactor: refactor expense and summary related class

--- lib/expense/daily_expense_summary.dart
+++ lib/expense/daily_expense_summary.dart
@@ -1,6 +1,6 @@
 import 'expense.dart';
 
-/// Expense といっているが、DailySummary を日付分だけ保持するクラス
+/// Expense といっているが、DailySummary を日付分だけ保持するクラス。
 class MonthlyExpense {
   const MonthlyExpense({
     required this.year,
@@ -14,21 +14,15 @@ class MonthlyExpense {
 }
 
 /// Expense といっているが、日毎（カレンダーの各セル）の
-/// 日付、支出一覧、収入一覧を一緒にもつクラス
+/// 日付、支出一覧、収入一覧を一緒にもつクラス。
 class DailySummary {
   DailySummary({
     required this.day,
     required this.expenses,
-    required this.incomes,
     this.totalExpense = 0,
-    this.totalIncome = 0,
   });
 
   final int day;
   final List<Expense> expenses;
-  final List<Income> incomes;
   int totalExpense;
-  int totalIncome;
 }
-
-class Income {}
