07f0f198d59a537401297362bf8b69888d93230e
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-05-02 01:07:15 +0900

    feat: add calendar page base

--- lib/calendar/ui/calendar.dart
+++ lib/calendar/ui/calendar.dart
@@ -2,6 +2,10 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../auth/auth.dart';
+import '../../router/router.dart';
+import '../../router/router.gr.dart';
+
 @RoutePage()
 class CalendarPage extends ConsumerWidget {
   const CalendarPage({super.key});
@@ -10,6 +14,28 @@ class CalendarPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text(ref.watch(userIdProvider).value ?? 'ログインしていません。'),
+            ),
+            ListTile(
+              title: const Text('サインアウト'),
+              onTap: () async {
+                await ref.read(signOutProvider).call();
+                await ref
+                    .read(appRouterProvider)
+                    .replaceAll([const SignInRoute()]);
+              },
+            ),
+            // 他のリストアイテムをここに追加できます
+          ],
+        ),
+      ),
+    );
   }
 }
