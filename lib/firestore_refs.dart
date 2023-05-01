66cf27ac78d1613ab831a5c7e523d88cc82ba6a4
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-05-02 01:08:01 +0900

    feat: define firestore_refs with dynamic userId

--- lib/firestore_refs.dart
+++ lib/firestore_refs.dart
@@ -9,41 +9,48 @@ final _db = FirebaseFirestore.instance;
 final v2ExpenseRef = _db.collection('v2Expense');
 
 /// expenses コレクションの参照。
-final expensesRef = _db
-    .collection('v2Expense')
-    .doc('hWou6fxFY6MZcGFxpHKkYcH4Iym2')
-    .collection('expenses')
-    .withConverter(
-  fromFirestore: (ds, _) {
-    return Expense.fromDocumentSnapshot(ds);
-  },
-  toFirestore: (obj, _) {
-    final json = obj.toJson();
-    return json;
-  },
-);
+CollectionReference<Expense> expensesRef({required String userId}) => _db
+        .collection('v2Expense')
+        .doc(userId)
+        .collection('expenses')
+        .withConverter(
+      fromFirestore: (ds, _) {
+        return Expense.fromDocumentSnapshot(ds);
+      },
+      toFirestore: (obj, _) {
+        final json = obj.toJson();
+        return json;
+      },
+    );
 
 /// expense ドキュメントの参照。
-DocumentReference<Expense> expenseRef({required String expenseId}) =>
-    expensesRef.doc(expenseId);
+DocumentReference<Expense> expenseRef({
+  required String userId,
+  required String expenseId,
+}) =>
+    expensesRef(userId: userId).doc(expenseId);
 
 /// expenseCategories コレクションの参照。
-final expenseCategoriesRef = _db
-    .collection('v2ExpenseCategory')
-    .doc('hWou6fxFY6MZcGFxpHKkYcH4Iym2')
-    .collection('expenseCategories')
-    .withConverter(
-  fromFirestore: (ds, _) {
-    return ExpenseCategory.fromDocumentSnapshot(ds);
-  },
-  toFirestore: (obj, _) {
-    final json = obj.toJson();
-    return json;
-  },
-);
+CollectionReference<ExpenseCategory> expenseCategoriesRef({
+  required String userId,
+}) =>
+    _db
+        .collection('v2ExpenseCategory')
+        .doc(userId)
+        .collection('expenseCategories')
+        .withConverter(
+      fromFirestore: (ds, _) {
+        return ExpenseCategory.fromDocumentSnapshot(ds);
+      },
+      toFirestore: (obj, _) {
+        final json = obj.toJson();
+        return json;
+      },
+    );
 
 /// expenseCategory ドキュメントの参照。
 DocumentReference<ExpenseCategory> expenseCategoryRef({
+  required String userId,
   required String expenseCategoryId,
 }) =>
-    expenseCategoriesRef.doc(expenseCategoryId);
+    expenseCategoriesRef(userId: userId).doc(expenseCategoryId);
