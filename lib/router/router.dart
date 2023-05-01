d8a395a67fe153c1ed9f59171efbcd62b10717f7
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-05-02 01:07:26 +0900

    feat: define routes with auth guard

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,16 +1,41 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../calendar/ui/calendar.dart';
+import '../sign_in/ui/sign_in.dart';
 import 'router.gr.dart';
+
+final appRouterProvider = Provider.autoDispose<AppRouter>((_) => AppRouter());
 
 @AutoRouterConfig()
 class AppRouter extends $AppRouter {
   @override
   final List<AutoRoute> routes = [
     RedirectRoute(path: '/', redirectTo: CalendarPage.path),
+    AutoRoute(path: SignInPage.path, page: SignInRoute.page),
     AutoRoute(
       path: CalendarPage.path,
       page: CalendarRoute.page,
+      guards: [_AuthGuard()],
     ),
   ];
+}
+
+/// ログインチェックを行う [AutoRouteGuard]。
+class _AuthGuard extends AutoRouteGuard {
+  @override
+  void onNavigation(NavigationResolver resolver, StackRouter router) {
+    final authenticated = FirebaseAuth.instance.currentUser != null;
+    if (authenticated) {
+      if (resolver.route.name == SignInRoute.name) {
+        router.replaceAll([const CalendarRoute()]);
+      } else {
+        resolver.next();
+      }
+      return;
+    } else {
+      router.push<void>(const SignInRoute());
+    }
+  }
 }
--- lib/router/router.gr.dart
+++ lib/router/router.gr.dart
@@ -8,27 +8,34 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:auto_route/auto_route.dart' as _i3;
 import 'package:flutter_account_book/calendar/ui/calendar.dart' as _i1;
+import 'package:flutter_account_book/sign_in/ui/sign_in.dart' as _i2;
 
-abstract class $AppRouter extends _i2.RootStackRouter {
+abstract class $AppRouter extends _i3.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i2.PageFactory> pagesMap = {
+  final Map<String, _i3.PageFactory> pagesMap = {
     CalendarRoute.name: (routeData) {
-      return _i2.AutoRoutePage<dynamic>(
+      return _i3.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i1.CalendarPage(),
       );
-    }
+    },
+    SignInRoute.name: (routeData) {
+      return _i3.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i2.SignInPage(),
+      );
+    },
   };
 }
 
 /// generated route for
 /// [_i1.CalendarPage]
-class CalendarRoute extends _i2.PageRouteInfo<void> {
-  const CalendarRoute({List<_i2.PageRouteInfo>? children})
+class CalendarRoute extends _i3.PageRouteInfo<void> {
+  const CalendarRoute({List<_i3.PageRouteInfo>? children})
       : super(
           CalendarRoute.name,
           initialChildren: children,
@@ -36,5 +43,19 @@ class CalendarRoute extends _i2.PageRouteInfo<void> {
 
   static const String name = 'CalendarRoute';
 
-  static const _i2.PageInfo<void> page = _i2.PageInfo<void>(name);
+  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i2.SignInPage]
+class SignInRoute extends _i3.PageRouteInfo<void> {
+  const SignInRoute({List<_i3.PageRouteInfo>? children})
+      : super(
+          SignInRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SignInRoute';
+
+  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
 }
