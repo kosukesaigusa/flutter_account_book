f860f2a428dc8c936054b0db6fa033d95029547c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-05-01 14:11:38 +0900

    build: specify firebase project in main.dart

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -4,9 +4,11 @@ import 'package:flutter_account_book/app.dart';
 import 'package:flutter_account_book/app_multi_provider.dart';
 import 'package:ks_flutter_commons/ks_flutter_commons.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     const RootWidget(
       child: AppMultiProvider(
